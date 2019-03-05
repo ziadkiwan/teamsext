a5fdd9b6cfd6621b1fd22ee43ba7ba4ab3e4e081
ziad-laptop <ziad_kiwan_1992@hotmail.com>
2019-03-05 10:04:00 +0200

    Added the app_config.py file

---
+++ app_config.py
@@ -0,0 +1,5 @@
+#### PLEASE FILL THE IFNORMATION HERE####
+client_id = "<<your_client_id>>"
+client_secret = "<<your_client_secret>>"
+app_integration_url_test = "<<your_api_integration_test>>"
+redirect_url = "https://github.com/ziadkiwan/teamsext"
